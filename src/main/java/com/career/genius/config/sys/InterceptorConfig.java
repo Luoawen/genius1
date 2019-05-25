13ca9fda3ed61cdf2b70d580478da89b6ecdcf86
Marker <luoqingwen@foxmail.com>
2019-05-25 15:07:32 +0800

    mvn 配置

---
+++ src/main/java/com/career/genius/config/sys/InterceptorConfig.java
@@ -0,0 +1,51 @@
+package com.career.genius.config.sys;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 请描述该类
+ *
+ * @author LZM
+ * @since 2018-11-21 18:51
+ **/
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("*")
+//               .allowedHeaders("*")
+//                .maxAge(3600)
+//                .allowCredentials(true);
+//    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+//
+//    /**
+//     * 文件上传配置
+//     *
+//     * @return
+//     */
+//    @Bean
+//    public MultipartConfigElement multipartConfigElement() {
+//        MultipartConfigFactory factory = new MultipartConfigFactory();
+//        //文件最大
+//        factory.setMaxFileSize(DataSize.ofMegabytes(40)); //KB,MB
+//        /// 设置总上传数据总大小
+//        factory.setMaxRequestSize(DataSize.ofMegabytes(40));
+//        return factory.createMultipartConfig();
+//    }
+}
--- src/main/java/com/career/genius/controller/auth/AuthController.java
+++ src/main/java/com/career/genius/controller/auth/AuthController.java
@@ -6,11 +6,13 @@ import com.career.genius.config.Exception.GeniusException;
 import com.career.genius.config.auth.vo.UserReqVo;
 import com.usm.enums.CodeEnum;
 import com.usm.vo.BaseResultDto;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -28,20 +30,21 @@ import javax.validation.Valid;
 @RestController
 @Slf4j
 @RequestMapping(value = "/users")
+@ApiModel(value = "用户接口")
 public class AuthController {
 
     @Autowired
     AuthApplication authApplication;
 
     @ApiOperation(value = "发送短信验证码")
-    @RequestMapping(value = "/message")
+    @RequestMapping(value = "/message",method = RequestMethod.POST)
     public BaseResultDto sendMsgCode(HttpServletRequest request, @RequestBody UserReqVo vo) throws Exception{
         authApplication.sendSms(request,vo.getPhone());
         return new BaseResultDto(CodeEnum.Success.getCode(),"短信发送成功");
     }
 
-    @ApiOperation(value = "发送短信验证码")
-    @RequestMapping(value = "/user")
+    @ApiOperation(value = "注册/登陆")
+    @RequestMapping(value = "/user",method = RequestMethod.POST)
     public BaseResultDto signin(HttpServletRequest request, @Valid @RequestBody SysUserReqVo vo) throws GeniusException {
         HttpSession session = request.getSession();
         authApplication.registOrLogin(vo,session);
